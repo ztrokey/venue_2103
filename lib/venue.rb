15c0b0b77f27d49737eb9e5f0fd018dc1401e8c1
Zach.Trokey <ztrokey@gmail.com>
2021-03-24 14:59:52 -0600

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,17 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons.push(patron)
+  end
+
+  def yell_at_patrons
+    @patrons.map { |patron| patron.upcase}
+  end
 end
